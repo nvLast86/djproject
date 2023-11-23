3bfaf1ce4c1bc0335344170080fff59fce30100e
nvLast86 <n.lastenko@gmail.com>
2023-11-23 09:54:29 +0600

    2 models has been created

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(verbose_name='Изображение')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    price = models.DecimalField(decimal_places=2, max_digits=7, verbose_name='Цена')
+    create_date = models.DateTimeField(verbose_name='Дата создания')
+    last_change_date = models.DateTimeField(verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
