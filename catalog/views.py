338ce4a06a69c5029d8bfd94e7c3773d04305783
nvLast86 <n.lastenko@gmail.com>
2023-12-01 20:48:34 +0600

    view.py has been changed

--- catalog/views.py
+++ catalog/views.py
@@ -9,7 +9,7 @@ def home(request):
 def contacts(request):
     if request.method == 'POST':
         name = request.POST.get('name')
-        phone = request.POST.get('phone')
+        email = request.POST.get('email')
         message = request.POST.get('message')
-        print(f'{name} {phone} {message}')
+        print(f'{name} {email} {message}')
     return render(request, 'catalog/contacts.html')
