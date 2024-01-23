3dff2945fae2dcaad584ef8336669fe1d45d26b8
nvLast86 <n.lastenko@gmail.com>
2024-01-23 12:12:30 +0600

    admin.py has been updated

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'first_name', 'last_name', 'country')
+    list_filter = ('id', 'email', 'first_name', 'last_name', 'country')
+    list_display_links = ('id', 'email', 'first_name', 'last_name', 'country')
