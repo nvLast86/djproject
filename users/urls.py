ba0da2359ea490a0529a35568e3f0d3948487993
nvLast86 <n.lastenko@gmail.com>
2024-01-24 00:51:09 +0600

    urls has been fixed

--- users/urls.py
+++ users/urls.py
@@ -1,26 +1,16 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
+
 from users.apps import UsersConfig
-from users.views import ProfileUpdateView, generate_new_password, UserForgotPasswordView, \
-    UserPasswordResetConfirmView, UserRegisterView, EmailConfirmationSentView, UserConfirmEmailView, EmailConfirmedView, \
-    EmailConfirmationFailedView
+from users.views import RegisterView, ProfileView, VerifyEmailView, CustomPasswordResetView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/', UserRegisterView.as_view(), name='register'),
-
-    path('email-confirmation-sent/', EmailConfirmationSentView.as_view(), name='email_confirmation_sent'),
-    path('confirm-email/<str:uidb64>/<str:token>/', UserConfirmEmailView.as_view(), name='confirm_email'),
-    path('email-confirmed/', EmailConfirmedView.as_view(), name='email_confirmed'),
-    path('confirm-email-failed/', EmailConfirmationFailedView.as_view(), name='email_confirmation_failed'),
-
-
-    path('profile/', ProfileUpdateView.as_view(), name='profile'),
-    path('profile/genpassword', generate_new_password, name='generate_new_password'),
-    path('password-reset/', UserForgotPasswordView.as_view(), name='password_reset'),
-    path('set-new-password/<uidb64>/<token>/', UserPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-
-    ]
+    path('register/', RegisterView.as_view(), name='register'),
+    path('profile/<int:pk>/', ProfileView.as_view(), name='profile'),
+    path('verify_email/<str:uid>/<str:token>/', VerifyEmailView.as_view(), name='verify_email'),
+    path('password_reset/', CustomPasswordResetView.as_view(), name='password_reset'),
+]
