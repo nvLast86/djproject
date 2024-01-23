240e7496baad5e51e8bec472b74a381cb1036f73
nvLast86 <n.lastenko@gmail.com>
2024-01-23 15:02:45 +0600

    main urls.py has been updated

--- config/urls.py
+++ config/urls.py
@@ -22,5 +22,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls', namespace='catalog')),
-    path('', include('blog.urls', namespace='blog'))
+    path('', include('blog.urls', namespace='blog')),
+    path('', include('users.urls', namespace='users')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
