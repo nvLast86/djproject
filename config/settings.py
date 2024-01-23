8029a39d7d55fe8b1e57a56c7ac9792cff5d9c35
nvLast86 <n.lastenko@gmail.com>
2024-01-23 12:20:37 +0600

    settings.py has been updated

--- config/settings.py
+++ config/settings.py
@@ -133,3 +133,14 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 AUTH_USER_MODEL = 'users.User'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_URL = '/users/'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'django.sendmail@yandex.ru'
+EMAIL_HOST_PASSWORD = 'isvltcgnpajxxmrp'
+EMAIL_USE_SSL = True
