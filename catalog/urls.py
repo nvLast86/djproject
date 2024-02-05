0b93836edcad76cf163b7e9cab3313ecd4ed7a7e
nvLast86 <n.lastenko@gmail.com>
2024-02-05 08:38:37 +0600

    little fixes

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,17 +3,18 @@ from catalog import views
 from catalog.apps import CatalogConfig
 from catalog.views import (contacts, ProductListView, ProductDetailView, CategoryListView, HomeView, ProductCreateView,
                            ProductUpdateView, ProductDeleteView)
+from django.views.decorators.cache import cache_page, never_cache
 
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', cashe_page(60)(HomeView.as_view()), name='home'),
+    path('', cache_page(60)(HomeView.as_view()), name='home'),
     path('contacts/', contacts, name='contacts'),
-    path('categories/', cashe_page(60)(CategoryListView.as_view()), name='categories'),
+    path('categories/', cache_page(60)(CategoryListView.as_view()), name='categories'),
     path('products/', ProductListView.as_view(), name='products-list'),
-    path('products/<int:pk>/', cashe_page(60)(ProductDetailView.as_view()), name='product-detail'),
-    path('create/', never_cashe(ProductCreateView.as_view()), name='product-create'),
-    path('update/<int:pk>/', never_cashe(ProductUpdateView.as_view()), name='product-update'),
+    path('products/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='product-detail'),
+    path('create/', never_cache(ProductCreateView.as_view()), name='product-create'),
+    path('update/<int:pk>/', never_cache(ProductUpdateView.as_view()), name='product-update'),
     path('delete/<int:pk>/', ProductDeleteView.as_view(), name='product-delete'),
 ]
